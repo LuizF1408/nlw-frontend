fdc934eda98d94878eb042017225019ba79a419c
Luiz <liko.12@hotmail.com>
2020-11-14 20:42:09 -0300

    Updates

---
+++ precache-manifest.302a27cb04dc77cb9ff13782d89832e4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a4dab2d00ebc9972dc6531c31c3a23",
+    "url": "/nlw-frontend/index.html"
+  },
+  {
+    "revision": "d2e563e6c61199309f14",
+    "url": "/nlw-frontend/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "3a9b4c37310f04a8b460",
+    "url": "/nlw-frontend/static/css/main.356c1fda.chunk.css"
+  },
+  {
+    "revision": "d2e563e6c61199309f14",
+    "url": "/nlw-frontend/static/js/2.0d6e4af1.chunk.js"
+  },
+  {
+    "revision": "3c56d32f1a0e992453f57615992b86b8",
+    "url": "/nlw-frontend/static/js/2.0d6e4af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a9b4c37310f04a8b460",
+    "url": "/nlw-frontend/static/js/main.a04e0d98.chunk.js"
+  },
+  {
+    "revision": "17fd35ff11106b3c8d86",
+    "url": "/nlw-frontend/static/js/runtime-main.33158140.js"
+  },
+  {
+    "revision": "91c8d1af04a8fbdde51d2b16c5471b15",
+    "url": "/nlw-frontend/static/media/Ilustra02.91c8d1af.svg"
+  },
+  {
+    "revision": "1653c64706c95e2e85a07cc755abf8ba",
+    "url": "/nlw-frontend/static/media/Logo.1653c647.svg"
+  },
+  {
+    "revision": "658b83029253fe8e316b8f364db7b424",
+    "url": "/nlw-frontend/static/media/map-local.658b8302.svg"
+  }
+]);
