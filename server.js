f2485dd293489ee6dce72772a9ce469ff8deaea4
Luiz Fernando <liko.12@hotmail.com>
2021-02-08 21:21:08 -0300

    deploy heroku

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
-var http = require('http');
+var path = require('path');
 const express = require('express');
-
-
 const app = express();
+const publicPath = path.join(__dirname,'build');
+const port = process.env.PORT || 3000;
 
-app.set('port', process.env.PORT || 3000);
- 
-const server = app.listen(app.get('port'), function() {
-    console.log('listening on port ', server.address().port);
-  });
+app.use(express.static(publicPath));
 
-
-
-const baseDir = `${__dirname}/build/`
-app.use(express.static(`${baseDir}`))
-app.get('/',(req,res) => res.sendfile('index.html',{root:baseDir}))
-
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
+ app.listen(port, () => {
+    console.log('Server is up!');
+ });
 
 // const path = require('path');
 // const express = require('express');
